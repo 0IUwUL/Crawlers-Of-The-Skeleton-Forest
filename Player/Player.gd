b3025a1186611fe2bea5aa445006f35febda63df
0IUwUL <markaljonreales.com>
2022-06-13 00:49:01 +0800

    feat: animation for character (in progress for npc)

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,14 +8,31 @@ const ACCELARATION = 200
 #slide on stop
 const FRICTION = 50
 
+#var animationplayer = null
+
+#shortcut in godot so no need for _ready func
+onready var animationplayer = $AnimationPlayer
+
+#func _ready():
+	#animationplayer = $AnimationPlayer
+
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationplayer.play("Run_Right")
+		elif input_vector.x < 0:
+			animationplayer.play("Run_Left")
+		else:
+			animationplayer.play("Run_Right")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
 	else:
+		animationplayer.play("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 #	
 	velocity = move_and_slide(velocity)
