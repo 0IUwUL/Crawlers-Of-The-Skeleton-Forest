6569978bb6cf02992572ead4ee68946d1a1cc1bb
0IUwUL <markaljonreales.com>
2022-06-14 12:52:30 +0800

    feat: added animation tree for MC Part 6

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,14 +8,9 @@ const ACCELARATION = 200
 #slide on stop
 const FRICTION = 50
 
-#var animationplayer = null
-
-#shortcut in godot so no need for _ready func
 onready var animationplayer = $AnimationPlayer
-
-#func _ready():
-	#animationplayer = $AnimationPlayer
-
+onready var animationtree = $AnimationTree
+onready var animationstate = animationtree.get("parameters/playback")
 
 
 func _physics_process(delta):
@@ -23,16 +18,14 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
+	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationplayer.play("Run_Right")
-		elif input_vector.x < 0:
-			animationplayer.play("Run_Left")
-		else:
-			animationplayer.play("Run_Right")
+		animationtree.set("parameters/Idle/blend_position", input_vector)
+		animationtree.set("parameters/Run/blend_position", input_vector)
+		animationstate.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
 	else:
-		animationplayer.play("Idle")
+		animationstate.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 #	
 	velocity = move_and_slide(velocity)
