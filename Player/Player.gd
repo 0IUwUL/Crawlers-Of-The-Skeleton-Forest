561a1d61f3f65322bfef46fb539080cffdcffe86
0IUwUL <markaljonreales.com>
2022-06-14 15:41:06 +0800

    fix: fixed signs and ritual perspective bug

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,12 +8,27 @@ const ACCELARATION = 200
 #slide on stop
 const FRICTION = 50
 
+enum{
+	MOVE,
+	ATTACK
+}
+var state = MOVE
+
 onready var animationplayer = $AnimationPlayer
 onready var animationtree = $AnimationTree
 onready var animationstate = animationtree.get("parameters/playback")
 
+func _onready():
+	animationtree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -22,6 +37,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationtree.set("parameters/Idle/blend_position", input_vector)
 		animationtree.set("parameters/Run/blend_position", input_vector)
+		animationtree.set("parameters/Attack/blend_position", input_vector)
 		animationstate.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
 	else:
@@ -29,3 +45,9 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 #	
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("Attack"):
+		state=ATTACK
+
+func attack_state(delta):
+	animationstate.travel("Attack")
