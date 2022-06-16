95b94af4a0b81579c515641dcf01527787dcbbdd
0IUwUL <markaljonreales.com>
2022-06-16 18:35:07 +0800

    feat: added attacking slime animation

--- NPC/Slime.gd
+++ NPC/Slime.gd
@@ -8,15 +8,21 @@ onready var stats = $Stats
 onready var playerDetection = $PlayerDetection
 onready var sprite = $AnimatedSprite
 onready var hurtbox = $Hurtbox
+onready var animationtree = $AnimationTree
+onready var animationstate = animationtree.get("parameters/playback") 
 
 enum {
 	IDLE,
 	WANDER,
-	CHASE
+	CHASE,
+	ATTACK
 }
 
 var velocity = Vector2.ZERO
 var state = CHASE
+
+func _ready():
+	animationtree.active = true
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, stats.Friction*delta)
@@ -26,19 +32,23 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, stats.Friction*delta)
 			seek_player()
-			sprite.play("Idle")
+			animationstate.travel('Idle')
 		WANDER:
 			pass
 		CHASE:
-			sprite.play("Run")
+			animationstate.travel('Run')
 			var player = playerDetection.player
 			if player != null:
 				var direction = (player.global_position - global_position).normalized()
+				animationtree.set("parameters/Run/blend_position", direction)
+				animationtree.set("parameters/Idle/blend_position", direction)
+				animationtree.set("parameters/Attack/blend_position", direction)
+				animationtree.set("parameters/Damage/blend_position", direction)
 				velocity = velocity.move_toward(direction * stats.Speed, stats.Acceleration * delta)
 			else:
 				state = IDLE
-			
-			sprite.flip_h = velocity.x<0
+		ATTACK:
+			animationstate.travel("Attack")
 	velocity = move_and_slide(velocity)
 		
 func seek_player():
@@ -46,14 +56,17 @@ func seek_player():
 		state = CHASE
 	
 func _on_Hurtbox_area_entered(area):
-	sprite.play("Damage")
+	animationstate.travel("Damage")
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 125
 	hurtbox.create_hit_effect()
-	
-func _on_HitBox_area_entered(area):
-	sprite.play("Attack")
 
+func attack_Done():
+	var player = playerDetection.player
+	if player != null:
+		state = CHASE
+	else:
+		state = IDLE
 	
 func _on_Stats_no_health():
 	queue_free()
@@ -61,5 +74,5 @@ func _on_Stats_no_health():
 	get_parent().add_child(slimeDeath)
 	slimeDeath.global_position = global_position
 
-
-
+func _on_EnemyAttackAnimate_body_entered(body):
+	state = ATTACK
